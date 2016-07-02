c24d9b48c399778c2defd072c7f5ec3665f633b2
Carlos Peñas <carlos.penas@the-cocktail.com>
2016-07-02 12:57:38 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,1 @@
+listec2ip
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+from distutils.core import setup
+setup(
+  name = 'listec2ip',
+  scripts = ['listec2ip'], 
+  version = '0.1',
+  description = 'Reads EC2 IP info within an AWS account / region',
+  author = 'Carlos Peñas',
+  author_email = 'carlos.penas@the-cocktail.com',
+  url = 'https://github.com/theist/listec2ip',
+  download_url = 'https://github.com/theist/listec2ip/tarball/0.1',
+  keywords = ['ec2', 'aws', 'ip'],
+  classifiers = [],
+)
